b8944ca0bd4ec904652a61ccdebd1a8ab14de9f2
hisayukiandre <hisaboss@icloud.com>
2021-09-25 03:43:21 +0000

    secret_keyを変更

--- mb_coders/settings.py
+++ mb_coders/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-12jj(3b5*!4xotbs_2rfzw_1(vf&6c6dcv6+glqu^%ba7gpztj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,3 +122,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
