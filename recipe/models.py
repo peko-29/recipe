4b3b5401234552ce3292da36b38e532d6609addb
Matsumoto213 <yusuke.213@icloud.com>
2021-09-25 06:48:12 +0000

    DBのメニュー作成と アドミンパネルを追加しました

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length = 100)
+    cost = models.IntegerField()
+    time = models.IntegerField()
+    process = models.TextField()
+    protein = models.FloatField()
+    fat = models.FloatField()
+    carbohydrate = models.FloatField()
+    cal = models.FloatField()
+    img = models.ImageField()
+    
