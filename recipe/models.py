01fd7af917fee9cbadd756b1b497ea91cf949391
peko-29 <allegro.andante.629@icloud.com>
2021-09-29 12:07:59 +0000

    テーブルの更新

--- recipe/models.py
+++ recipe/models.py
@@ -12,18 +12,33 @@ class Menu(models.Model):
     cal = models.FloatField()
     img = models.ImageField()
     
+    def __str__(self):
+        return self.name
+    
 class Genre(models.Model):
     name = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return self.name
     
 class Small_Genre(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     name = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return self.name
 
 class Material(models.Model):
     small_genre = models.ForeignKey(Small_Genre, on_delete=models.CASCADE)
     name = models.CharField(max_length = 100)
     
+    def __str__(self):
+        return self.name
+        
 class Menu_Material(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     amount = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return self.amount
