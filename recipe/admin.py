4867f7e14dacc8fef9f62850744f8877c5156f7a
peko-29 <allegro.andante.629@icloud.com>
2021-09-29 11:48:12 +0000

    テーブルの更新

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, Genre, Small_Genre, Material, Menu_Material
 
 # Register your models here.
 admin.site.register(Menu)
+admin.site.register(Genre)
+admin.site.register(Small_Genre)
+admin.site.register(Material)
+admin.site.register(Menu_Material)
--- recipe/models.py
+++ recipe/models.py
@@ -12,3 +12,18 @@ class Menu(models.Model):
     cal = models.FloatField()
     img = models.ImageField()
     
+class Genre(models.Model):
+    name = models.CharField(max_length = 100)
+    
+class Small_Genre(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+
+class Material(models.Model):
+    small_genre = models.ForeignKey(Small_Genre, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+    
+class Menu_Material(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    amount = models.CharField(max_length = 100)
