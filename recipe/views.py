f23895cb36d40eaaacc82aab47d7a06950488fa0
Sakichi-ac <sachicken201@gmail.com>
2021-10-12 14:15:11 +0000

    materials_listの修正

--- recipe/views.py
+++ recipe/views.py
@@ -42,9 +42,10 @@ def index(request):
         materials_list = []
         for small_genre in small_genres_query_set:
             materials_query_set = small_genre.material_set.all()
-            materials_list.append(list(materials_query_set.values()))
+            materials_list.extend(list(materials_query_set.values()))
         # 抽出した辞書型のリストをモデルkeyの辞書型に格納
         materials_to_search = {'genres_list':genres_list, 'small_genres_list':small_genres_list, 'materials_list':materials_list}
+        print(materials_to_search)
         return render(request, 'recipe/index.html', materials_to_search)
 
 def detail(request, menu_id):
